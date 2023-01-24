750df6577a3c5607608f0a4698fc7afe2183925b
Anna Novak <77454079+anovak4@users.noreply.github.com>
2023-01-24 05:49:09 +0000

    clean up

--- Wordle.java
+++ Wordle.java
@@ -7,54 +7,53 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class Wordle implements KeyListener {
-    private JLabel[][] words;
-    private int wordIndex = 0;
-    private int letterIndex = 0;
+	private JLabel[][] words;
+	private int wordIndex = 0;
+	private int letterIndex = 0;
 	private String target;
 	private String[] targetArr;
 	private String[] correctness = {"a", "a", "a", "a", "a"}; 
 
-    public Wordle() {
+	public Wordle() {
 		int ran = (int) (Math.random() * 997) + 1; // range of file is 1-997
 		try(BufferedReader reader = new BufferedReader(new FileReader("words.txt"))) {
 			for (int i = 0; i < ran; i++)
-              reader.readLine();
-          	target = reader.readLine();
+				reader.readLine();
+			target = reader.readLine();
 		} catch(IOException e) {
-        	System.out.println(e);
-      	}
+			System.out.println(e);
+		}
 		targetArr = target.split("");
-		System.out.println(target);
 		
-        JFrame frame = new JFrame("Wordle");
+		JFrame frame = new JFrame("Wordle");
 
-        JPanel wordPanel = new JPanel();
-        wordPanel.setLayout(new GridLayout(6,5,10,13)); // rows, cols, hgap, vgap
-        
-        words = new JLabel[6][5];
-        for(int w = 0; w < words.length; w++) {
-            for(int l = 0; l < words[w].length; l++) {
-            	JLabel letter = new JLabel();
-                letter.setOpaque(true); //needed to be able to set the background
-                letter.setBackground(new Color(194, 194, 194));
-                letter.setForeground(Color.black);
-                letter.setPreferredSize(new Dimension(40,60));
-                letter.setFont(new Font("Arial", Font.BOLD, 40));
+		JPanel wordPanel = new JPanel();
+		wordPanel.setLayout(new GridLayout(6,5,10,13)); // rows, cols, hgap, vgap
+		
+		words = new JLabel[6][5];
+		for(int w = 0; w < words.length; w++) {
+			for(int l = 0; l < words[w].length; l++) {
+				JLabel letter = new JLabel();
+				letter.setOpaque(true); // needed to be able to set the background
+				letter.setBackground(new Color(194, 194, 194));
+				letter.setForeground(Color.black);
+				letter.setPreferredSize(new Dimension(40,60));
+				letter.setFont(new Font("Arial", Font.BOLD, 40));
 				letter.setHorizontalAlignment(SwingConstants.CENTER);
-                wordPanel.add(letter);
-                letter.addKeyListener(this);
-                words[w][l] = letter;
-            }
-        }
-
+				wordPanel.add(letter);
+				letter.addKeyListener(this);
+				words[w][l] = letter;
+			}
+		}
+	
 		frame.pack();
-        frame.add(wordPanel, BorderLayout.NORTH);
-        frame.setSize(300, 450);
+		frame.add(wordPanel, BorderLayout.NORTH);
+		frame.setSize(300, 450);
 		words[wordIndex][letterIndex].requestFocusInWindow(); 
-        frame.setVisible(true);    
-    }
+		frame.setVisible(true);    
+	}
 
-    public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e) {
 		if(e.getKeyChar() == '\b') {
 			// backspace
 			if(letterIndex > 0) {
@@ -73,13 +72,13 @@ public class Wordle implements KeyListener {
 				letterIndex++;
 			}
 		}
-    }
+	}
 
 	// only need these because I'm implementing KeyListener
-    public void keyPressed(KeyEvent e) {}
-    public void keyReleased(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 
-	private void checkAnswer(){
+	private void checkAnswer() {
 		Arrays.fill(correctness, "a");
 		for(int i = 0; i < words[wordIndex].length; i++) {
 			checkCorrect(i);
@@ -136,7 +135,7 @@ public class Wordle implements KeyListener {
 					correctness[index1] = "w";
 				} else {
 					correctness[index1] = "a";
-	                correctness[index2] = "w";
+	        correctness[index2] = "w";
 				}
 			}
 		}
@@ -153,5 +152,5 @@ public class Wordle implements KeyListener {
 	private boolean containsDuplicate(String str, char letter) {
 		return str.indexOf(letter) != str.lastIndexOf(letter);
 	}
-    
+
 }
